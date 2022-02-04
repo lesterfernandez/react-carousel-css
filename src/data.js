ef2fc9b7f89c459553e86cc8466a8500f6c8199c
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2022-02-03 20:47:35 -0500

    initial commit

--- src/data.js
+++ src/data.js
@@ -1,9 +1,24 @@
 const data = [
-  { src: "https://picsum.photos/200/200", title: "A mock heading" },
-  { src: "https://picsum.photos/200/200", title: "A mock heading" },
-  { src: "https://picsum.photos/200/200", title: "A mock heading" },
-  { src: "https://picsum.photos/200/200", title: "A mock heading" },
-  { src: "https://picsum.photos/200/200", title: "A mock heading" },
+  {
+    src: "https://i.picsum.photos/id/235/400/400.jpg?hmac=lhKy3NgJaHngfFJpUSThs5GCScgNjaqavtTVvLCiRRc",
+    title: "A nice view",
+  },
+  {
+    src: "https://i.picsum.photos/id/844/400/400.jpg?hmac=_oCcst4n0X6adjyA_hE9zPyLTADwKmYETga4tV-ocQE",
+    title: "Green Bamboo",
+  },
+  {
+    src: "https://i.picsum.photos/id/5/400/400.jpg?hmac=P8V7mQXwAUnx9rTeHzGEWnux2lS4iDnXuZQMmPdp6a8",
+    title: "dude on a laptop",
+  },
+  {
+    src: "https://i.picsum.photos/id/830/400/400.jpg?hmac=PTgxF7YOSn6WHgCetWwC3rZjxMcl69m0m9hUrV6nmTs",
+    title: "Noice Hills m8",
+  },
+  {
+    src: "https://i.picsum.photos/id/206/400/400.jpg?hmac=ZfNzmwb_G-cENYBcFfGB0f-xCi3wb6TBkE-4eQGt7ys",
+    title: "Sun shines bright",
+  },
 ];
 
 export default data;
