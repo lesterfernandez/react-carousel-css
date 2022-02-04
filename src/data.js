bd29d4c555ec42b9882bcd3d6fb4da131e541fc7
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2022-02-03 19:29:26 -0500

    initial commit

---
+++ src/data.js
@@ -0,0 +1,9 @@
+const data = [
+  { src: "https://picsum.photos/200/200", title: "A mock heading" },
+  { src: "https://picsum.photos/200/200", title: "A mock heading" },
+  { src: "https://picsum.photos/200/200", title: "A mock heading" },
+  { src: "https://picsum.photos/200/200", title: "A mock heading" },
+  { src: "https://picsum.photos/200/200", title: "A mock heading" },
+];
+
+export default data;
